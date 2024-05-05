bd10f57bcb87e34898631853493384828f61de2e
Vijay Harsha Made <87754774+vijayharshamade@users.noreply.github.com>
2024-05-05 23:03:12 +0530

    Update index.js

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -3,17 +3,22 @@ import './index.css'
 const TodoItem = props => {
   const {todoDetails, deleteTodo} = props
   const {id, title} = todoDetails
-  
+
   const onDeleteTodo = () => {
-    deleteTodo()
+    // FIX5 :The id of the todo has to be sent as an argument to the callback function
+    deleteTodo(id)
   }
 
   return (
+    // FIX6: The HTML elements should be wrapped inside a single element when returning
+    <li className="todo-item">
       <p className="title">{title}</p>
       <button type="button" className="delete-btn" onClick={onDeleteTodo}>
         Delete
       </button>
+    </li>
   )
 }
 
 export default TodoItem
+
